5901f918e774f2b9d2474d505c24ff2cede4d21e
JoasE <32096708+JoasE@users.noreply.github.com>
2020-07-29 15:07:24 +0200

    Initial commit

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Produces("text/plain")]
+        public string Get(Test test)
+        {
+            return "It's going to be rainy";
+        }
+    }
+
+    public class Test
+    {
+        [Required]
+        public string Test2 { get; set; }
+    }
+}
